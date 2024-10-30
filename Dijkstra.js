ad37021f99d009169cad32feb3fa5767e5d76ac7
SantiZai <santyagozaidan@gmail.com>
2024-10-30 17:36:26 -0300

    docs

--- Dijkstra.js
+++ Dijkstra.js
@@ -1,38 +1,86 @@
-export function createGraph(V,E){
-    // V - Number of vertices in graph
-    // E - Number of edges in graph (u,v,w)
-    let adj_list = []; // Adjacency list
-    for(let i = 0 ; i < V ; i++){
+/**
+ * @fileoverview 
+ * Implementación del algoritmo de Dijkstra para encontrar el camino más corto en un grafo ponderado.
+ * Incluye funciones para crear el grafo y ejecutar el algoritmo de Dijkstra.
+ * 
+ * @author Santiago
+ * @license MIT
+ * @see {@link https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm} Para más información sobre el algoritmo de Dijkstra
+ */
+
+/**
+ * Crea un grafo no dirigido usando una lista de adyacencia
+ * 
+ * @param {number} V - Número de vértices en el grafo (0 a V-1)
+ * @param {Array<Array<number>>} E - Array de aristas donde cada arista es [origen, destino, peso]
+ * @returns {AdjacencyList} Lista de adyacencia donde cada elemento es un array de arrays [vértice_destino, peso]
+ * 
+ * @example
+ * const V = 9;
+ * const E = [[0,1,4], [0,7,8]];  // [origen, destino, peso]
+ * const graph = createGraph(V, E);
+ */
+export function createGraph(V, E) {
+    // Inicializa la lista de adyacencia
+    let adj_list = [];
+    for(let i = 0; i < V; i++) {
         adj_list.push([]);
     }
-    for(let i = 0 ; i < E.length ; i++){
-        adj_list[E[i][0]].push([E[i][1],E[i][2]]);
-        adj_list[E[i][1]].push([E[i][0],E[i][2]]);
+
+    // Agrega cada arista en ambas direcciones (grafo no dirigido)
+    for(let i = 0; i < E.length; i++) {
+        adj_list[E[i][0]].push([E[i][1], E[i][2]]);
+        adj_list[E[i][1]].push([E[i][0], E[i][2]]);
     }
+
     return adj_list;
 }
 
+/**
+ * Implementa el algoritmo de Dijkstra para encontrar las distancias más cortas desde un vértice origen
+ * 
+ * @param {AdjacencyList} graph - Grafo representado como lista de adyacencia
+ * @param {number} V - Número de vértices en el grafo
+ * @param {number} src - Vértice origen (0 a V-1)
+ * @returns {Array<Array<number>>} Array donde cada elemento es [distancia_mínima, vértice_anterior]
+ * 
+ * @example
+ * const src = 0;
+ * const distances = djikstra(graph, V, src);
+ * // distances[i][0] es la distancia más corta desde src hasta el vértice i
+ * // distances[i][1] es el vértice anterior en el camino más corto hasta i
+ */
 export function djikstra(graph, V, src) {
+    // Array de vértices visitados
     let vis = Array(V).fill(0);
+    
+    // Inicializa distancias como infinito (10000) y vértice anterior como -1
     let dist = [];
-    for(let i=0;i<V;i++)
-        dist.push([10000,-1]);
-    dist[src][0] = 0;
+    for(let i = 0; i < V; i++) {
+        dist.push([10000, -1]);
+    }
+    dist[src][0] = 0;  // Distancia al origen es 0
 
-    for(let i=0;i<V-1;i++){
+    // Procesa todos los vértices
+    for(let i = 0; i < V-1; i++) {
+        // Encuentra el vértice no visitado con menor distancia
         let mn = -1;
-        for(let j=0;j<V;j++){
-            if(vis[j]===0){
-                if(mn===-1 || dist[j][0]<dist[mn][0])
+        for(let j = 0; j < V; j++) {
+            if(vis[j] === 0) {
+                if(mn === -1 || dist[j][0] < dist[mn][0]) {
                     mn = j;
+                }
             }
         }
 
+        // Marca el vértice como visitado
         vis[mn] = 1;
-        for(let j=0;j<graph[mn].length;j++){
+
+        // Actualiza las distancias de los vértices adyacentes
+        for(let j = 0; j < graph[mn].length; j++) {
             let edge = graph[mn][j];
-            if(vis[edge[0]]===0 && dist[edge[0]][0]>dist[mn][0]+edge[1]){
-                dist[edge[0]][0] = dist[mn][0]+edge[1];
+            if(vis[edge[0]] === 0 && dist[edge[0]][0] > dist[mn][0] + edge[1]) {
+                dist[edge[0]][0] = dist[mn][0] + edge[1];
                 dist[edge[0]][1] = mn;
             }
         }
@@ -41,11 +89,12 @@ export function djikstra(graph, V, src) {
     return dist;
 }
 
-let src = 0;
-let V = 9;
-let E = [[0,1,4], [0,7,8], [1,7,11], [1,2,8], [7,8,7], [6,7,1], [2,8,2],
+// Ejemplo de uso
+const src = 0;
+const V = 9;
+const E = [[0,1,4], [0,7,8], [1,7,11], [1,2,8], [7,8,7], [6,7,1], [2,8,2],
     [6,8,6], [5,6,2], [2,5,4], [2,3,7], [3,5,14], [3,4,9], [4,5,10]];
 
-let graph = createGraph(V,E);
-let distances = djikstra(graph,V,0);
+const graph = createGraph(V, E);
+const distances = djikstra(graph, V, 0);
 console.log(distances);
--- Graph.js
+++ Graph.js
@@ -1,20 +1,65 @@
-export function createGraph(V, E){
+/**
+ * @fileoverview 
+ * Este archivo contiene funciones para la creación y manipulación de grafos
+ * utilizando listas de adyacencia como estructura de datos principal.
+ * 
+ * @author Santiago
+ * @license MIT
+ * 
+ * @see {@link https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)} Para más información sobre teoría de grafos
+ * @see {@link https://en.wikipedia.org/wiki/Adjacency_list} Para más información sobre listas de adyacencia
+ */
+
+/**
+ * @interface Edge
+ * Representa una arista en el grafo
+ * @property {number} source - Vértice origen
+ * @property {number} target - Vértice destino
+ * @property {number} weight - Peso de la arista
+ */
+
+/**
+ * @const {number}
+ * Número de vértices en el grafo (0 a V inclusive)
+ */
+const V = 5;
+
+/**
+ * @const {Array<Array<number>>}
+ * Array de aristas donde cada arista es [origen, destino, peso]
+ */
+const E = [[1,2,3], [1,4,2], [3,5,1], [3,4,3]];
+
+/**
+ * Crea un grafo no dirigido utilizando una lista de adyacencia.
+ * 
+ * @param {number} V - Número de vértices en el grafo (0 a V inclusive)
+ * @param {Array<Array<number>>} E - Array de aristas donde cada arista es un array de 3 elementos [origen, destino, peso]
+ * @returns {AdjacencyList} Lista de adyacencia donde cada elemento es un array de arrays [vértice_destino, peso]
+ * 
+ * @example
+ * const V = 5;
+ * const E = [[1,2,3], [1,4,2], [3,5,1], [3,4,3]];
+ * const graph = createGraph(V, E);
+ * // Retorna una lista de adyacencia donde graph[i] contiene los vecinos del vértice i
+ * // Cada vecino se representa como [vértice_destino, peso_arista]
+ */
+export function createGraph(V, E) {
     let adjacency_list = [];
 
-    for (let i=0; i<=V; i++){
+    // Inicializa la lista de adyacencia vacía para cada vértice
+    for (let i = 0; i <= V; i++) {
         adjacency_list.push([]);
     }
 
-    for (let i=0; i<E.length; i++){
-        adjacency_list[ E[i][0] ].push( [E[i][1], E[i][2]] );
-        adjacency_list[ E[i][1] ].push( [E[i][0], E[i][2]] );
+    // Agrega cada arista en ambas direcciones (grafo no dirigido)
+    for (let i = 0; i < E.length; i++) {
+        adjacency_list[E[i][0]].push([E[i][1], E[i][2]]);
+        adjacency_list[E[i][1]].push([E[i][0], E[i][2]]);
     }
 
     return adjacency_list;
 }
-
-const V = 5;
-const E = [ [1,2,3], [1,4,2], [3,5,1], [3,4,3] ];
 
 let graph = createGraph(V, E);
 
